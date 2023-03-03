da223d702f5b96e45f8d3a71633bfbfdb403a9f6
nijineko <nijinekoyo@outlook.com>
2023-03-04 02:23:42 +0800

    完成程序

--- internal/Flag/Flag.go
+++ internal/Flag/Flag.go
@@ -13,6 +13,9 @@ import "flag"
 type Flag struct {
 	Version          string // 数据包版本
 	OriginalFileSave bool   // 是否以原始文件的名字和路径保存
+	AssetBundls      bool   // 下载AssetBundls文件
+	TableBundles     bool   // 下载TableBundles文件
+	MediaResources   bool   // 下载MediaResources文件
 }
 
 var Data Flag
@@ -25,11 +28,23 @@ func Init() error {
 	// 参数解析
 	Version := flag.String("data_version", "", "指定数据包版本")
 	OriginalFileSave := flag.Bool("original_file_save", false, "是否以原始文件的名字和路径保存")
+	AssetBundls := flag.Bool("asset_bundls", false, "下载AssetBundls文件")
+	TableBundles := flag.Bool("table_bundles", false, "下载TableBundles文件")
+	MediaResources := flag.Bool("media_resources", false, "下载MediaResources文件")
 	flag.Parse()
 
 	// 参数写入变量
 	Data.Version = *Version
 	Data.OriginalFileSave = *OriginalFileSave
+	if *AssetBundls || *TableBundles || *MediaResources {
+		Data.AssetBundls = *AssetBundls
+		Data.TableBundles = *TableBundles
+		Data.MediaResources = *MediaResources
+	} else {
+		Data.AssetBundls = true
+		Data.TableBundles = true
+		Data.MediaResources = true
+	}
 
 	return nil
 }
--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 /*
  * @Author: NyanCatda
  * @Date: 2023-03-03 22:42:20
- * @LastEditTime: 2023-03-03 22:46:20
+ * @LastEditTime: 2023-03-04 01:27:06
  * @LastEditors: nijineko
  * @Description: main file
  * @FilePath: \DataDownload\main.go
  */
 package main
 
-import "DataDownload/internal/Flag"
+import (
+	"DataDownload/internal/Catalog"
+	"DataDownload/internal/Download"
+	"DataDownload/internal/Flag"
+	"DataDownload/internal/MateData"
+	"fmt"
+	"os"
+	"path"
+)
 
 func init() {
 	// 初始化参数
@@ -19,5 +27,70 @@ func init() {
 }
 
 func main() {
+	// 读取元数据
+	Mate, err := MateData.Get(Flag.Data.Version)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("元数据获取失败，可能是版本号不正确或者网络问题")
+		os.Exit(1)
+	}
 
+	AddressablesCatalogUrlRoot := Mate.ConnectionGroups[0].OverrideConnectionGroups[len(Mate.ConnectionGroups[0].OverrideConnectionGroups)-1].AddressablesCatalogURLRoot
+
+	// 下载AssetBundls资源
+	if Flag.Data.AssetBundls {
+		AssetBundlsCataLog, err := Catalog.GetAssetBundls(AddressablesCatalogUrlRoot)
+		if err != nil {
+			panic(err)
+		}
+		var AssetBundlsSavePath string
+		if Flag.Data.OriginalFileSave {
+			AssetBundlsSavePath = path.Join("com.YostarJP.BlueArchive", "files", "AssetBundls")
+		} else {
+			AssetBundlsSavePath = path.Join("com.YostarJP.BlueArchive", "AssetBundls")
+		}
+		err = Download.AssetBundls(AddressablesCatalogUrlRoot, AssetBundlsCataLog, AssetBundlsSavePath)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// 下载TableBundles资源
+	if Flag.Data.TableBundles {
+		TableBundlesCataLog, err := Catalog.GetTableBundles(AddressablesCatalogUrlRoot)
+		if err != nil {
+			panic(err)
+		}
+		var TableBundlesSavePath string
+		if Flag.Data.OriginalFileSave {
+			TableBundlesSavePath = path.Join("com.YostarJP.BlueArchive", "files", "TableBundles")
+		} else {
+			TableBundlesSavePath = path.Join("com.YostarJP.BlueArchive", "TableBundles")
+		}
+		err = Download.TableBundles(AddressablesCatalogUrlRoot, TableBundlesCataLog, TableBundlesSavePath)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// 下载MediaResources资源
+	if Flag.Data.MediaResources {
+		MediaResourcesCataLog, err := Catalog.GetMediaResources(AddressablesCatalogUrlRoot)
+		if err != nil {
+			panic(err)
+		}
+		var MediaResourcesSavePath string
+		if Flag.Data.OriginalFileSave {
+			MediaResourcesSavePath = path.Join("com.YostarJP.BlueArchive", "files", "MediaPatch")
+		} else {
+			MediaResourcesSavePath = path.Join("com.YostarJP.BlueArchive", "MediaResources")
+		}
+		err = Download.MediaResources(AddressablesCatalogUrlRoot, MediaResourcesCataLog, MediaResourcesSavePath)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Println("下载完成")
+	os.Exit(0)
 }
