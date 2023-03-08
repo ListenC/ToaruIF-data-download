4054413e0e34d809c6cc23692da39a3bb8184b67
nijineko <nijinekoyo@outlook.com>
2023-03-08 18:01:42 +0800

    增加判断资源服务器是否可用

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
  * @Author: NyanCatda
  * @Date: 2023-03-03 22:42:20
- * @LastEditTime: 2023-03-04 01:27:06
+ * @LastEditTime: 2023-03-08 18:00:37
  * @LastEditors: nijineko
  * @Description: main file
  * @FilePath: \DataDownload\main.go
@@ -12,10 +12,12 @@ import (
 	"BlueArchiveDataDownload/internal/Catalog"
 	"BlueArchiveDataDownload/internal/Download"
 	"BlueArchiveDataDownload/internal/Flag"
+	"BlueArchiveDataDownload/internal/HTTP"
 	"BlueArchiveDataDownload/internal/MateData"
 	"fmt"
 	"os"
 	"path"
+	"time"
 )
 
 func main() {
@@ -34,6 +36,23 @@ func main() {
 	}
 
 	AddressablesCatalogUrlRoot := Mate.ConnectionGroups[0].OverrideConnectionGroups[len(Mate.ConnectionGroups[0].OverrideConnectionGroups)-1].AddressablesCatalogURLRoot
+
+	// 判断资源服务器是否可用
+	for {
+		_, Response, err := HTTP.Get(AddressablesCatalogUrlRoot + Catalog.TableBundlesCataLogPath)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("资源服务器信息获取失败")
+			os.Exit(1)
+		}
+		if Response.StatusCode != 200 {
+			fmt.Println("资源服务器不可用，将会每隔5秒尝试连接一次，直到成功为止")
+			time.Sleep(5 * time.Second)
+			continue
+		} else {
+			break
+		}
+	}
 
 	// 下载AssetBundls资源
 	if Flag.Data.AssetBundls {
